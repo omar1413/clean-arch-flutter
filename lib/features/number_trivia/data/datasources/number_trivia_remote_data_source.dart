7aa546c60b75a9d4dddb61995dc92a99eacde07c
omar osama <omarosama200100@gmail.com>
2019-11-27 01:38:55 +0200

    finish remote data source

--- lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -1,7 +1,36 @@
+import 'dart:convert';
+
+import 'package:clean_arch/core/base/api.dart';
+import 'package:clean_arch/core/error/exeptions.dart';
 import 'package:clean_arch/features/number_trivia/data/models/number_trivia_model.dart';
 
 abstract class NumberTriviaRemoteDataSource {
   Future<NumberTriviaModel> getConcreteNumberTrivia(int number);
 
   Future<NumberTriviaModel> getRandomNumberTrivia();
+}
+
+class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
+  final Api api;
+
+  NumberTriviaRemoteDataSourceImpl(this.api);
+
+  @override
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number) =>
+      getNumberOrRandom('${EndPoints.number}$number');
+
+  @override
+  Future<NumberTriviaModel> getRandomNumberTrivia() =>
+      getNumberOrRandom(EndPoints.random);
+
+  Future<NumberTriviaModel> getNumberOrRandom(String endpoint) async {
+    final res = await api.get(
+      Api.BASE_URL + endpoint,
+      headers: {'Content-Type': 'application/json'},
+    );
+    if (res.statusCode != 200) {
+      throw ServerException();
+    }
+    return NumberTriviaModel.fromJson(json.decode(res.body));
+  }
 }
---
+++ test/core/base/api_test.dart
@@ -0,0 +1,34 @@
+import 'package:clean_arch/core/base/api.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/mockito.dart';
+
+class MockHttpClient extends Mock implements http.Client {}
+
+void main() {
+  MockHttpClient mockClient;
+  ApiImpl api;
+
+  setUp(() {
+    mockClient = MockHttpClient();
+    api = ApiImpl(mockClient);
+  });
+
+  group('get', () {
+    test('should forward call to mockCLinet.get', () async {
+      Future<http.Response> tFutureRes =
+          Future.value(http.Response('test body', 200));
+
+      final tUrl = 'endpoint';
+      final tHeaders = {'test': 'test'};
+
+      when(mockClient.get(any, headers: anyNamed('headers')))
+          .thenAnswer((_) => tFutureRes);
+
+      final result = api.get(tUrl, headers: tHeaders);
+
+      expect(result, tFutureRes);
+      verify(mockClient.get(tUrl, headers: tHeaders));
+    });
+  });
+}
